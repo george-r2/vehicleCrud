3a41e603eabec47978e7763468ae90a6e9c84ad5
george_r2 <arthur.mendoza.25@gmail.com>
2025-04-29 00:17:01 -0600

    First commit, blank project

---
+++ src/test/java/com/example/vehicleCrud/VehicleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicleCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
